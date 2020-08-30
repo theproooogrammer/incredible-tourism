50b8114682f82ba8b829967443a2d11dd9206227
kishor Chintalchere <kishorchintalchere@gmail.com>
2020-08-30 14:56:59 +0530

    Ready to deploy

--- src/main/java/com/tourism/config/AppConfig.java
+++ src/main/java/com/tourism/config/AppConfig.java
@@ -18,6 +18,7 @@ import static org.hibernate.cfg.Environment.*;
 @Configuration
 @EnableTransactionManagement
 @PropertySource("classpath:db.properties")
+@PropertySource("classpath:application.properties")
 @ComponentScans(value = { @ComponentScan("com.tourism.dao"), @ComponentScan("com.tourism.service") })
 public class AppConfig {
 
@@ -30,13 +31,13 @@ public class AppConfig {
 		LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
 
 		Properties properties = new Properties();
-		properties.put(DRIVER, environment.getProperty("mysql.driver"));
-		properties.put(URL, environment.getProperty("mysql.url"));
-		properties.put(USER, environment.getProperty("mysql.user"));
-		properties.put(PASS, environment.getProperty("mysql.password"));
+		properties.put(DRIVER, environment.getProperty("spring.datasource.driverClassName"));
+		properties.put(URL, environment.getProperty("spring.datasource.url"));
+		properties.put(USER, environment.getProperty("spring.datasource.username"));
+		properties.put(PASS, environment.getProperty("spring.datasource.password"));
 
-		properties.put(SHOW_SQL, environment.getProperty("hibernate.show_sql"));
-		properties.put(HBM2DDL_AUTO, environment.getProperty("hibernate.hbm2ddl.auto"));
+		properties.put(SHOW_SQL, environment.getProperty("spring.jpa.show-sql"));
+		properties.put(HBM2DDL_AUTO, environment.getProperty("spring.jpa.hibernate.ddl-auto"));
 
 		properties.put(C3P0_MIN_SIZE, environment.getProperty("hibernate.c3p0.min_size"));
 		properties.put(C3P0_MAX_SIZE, environment.getProperty("hibernate.c3p0.max_size"));
